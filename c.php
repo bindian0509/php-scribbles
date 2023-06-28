aec2d4a687c932ff2c45456e6296fa069f92274e
Bharat Verma <bindian0509@gmail.com>
2023-06-28 23:10:35 +0530

    Some files

--- c.php
+++ c.php
@@ -1,2 +1,19 @@
 <?php
 
+class C {
+
+    public function printSomething() {
+        echo "Sometimes what I think about is you!";
+    }
+
+    public static function checkCalls () {
+        //self::printSomething();
+        $cObj = new C();
+        $cObj->printSomething();
+
+    }
+}
+
+
+C::checkCalls();
+
