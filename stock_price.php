d987df4ce04a498125e14e0170b6efbc51a5f635
Bharat V <bindian0509@gmail.com>
2023-09-28 16:43:39 +0530

    Sometimes co-pilot do more harm than good

--- stock_price.php
+++ stock_price.php
@@ -3,22 +3,25 @@
 
 function max_profit_single_transaction($prices) : int {
 
+    if(count($prices) < 2)
+        return 0;
+
     $max_profit = 0;
     $cheapest = $prices[0];
 
-    for ($i=0; $i < count($prices) - 1; $i++) {
+    for ($i=0; $i < count($prices); $i++) {
+
         $cheapest = min($prices[$i], $cheapest);
         $max_profit = max($max_profit, $prices[$i]-$cheapest);
     }
     return $max_profit;
 
 }
-
-$prices = [7,1,5,3,6,4];
-echo "Max Profit in the case of " . implode(",", $prices) . " is: ";
+$prices = [1,2,3,4,5,6];
+echo "\nMax Profit in the case of " . implode(",", $prices) . " is: ";
 echo max_profit_single_transaction($prices)."\n";
 
-$prices = [1,2,3,4,5,6];
+$prices = [7,1,5,3,6,4];
 echo "Max Profit in the case of " . implode(",", $prices) . " is: ";
 echo max_profit_single_transaction($prices)."\n";
 
